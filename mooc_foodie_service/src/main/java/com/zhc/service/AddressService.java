b431b8d187e6376e084d3ddfdabeba231d108ce0
couthz <344304180@qq.com>
2020-04-09 20:54:26 +0800

    完成地址

---
+++ mooc_foodie_service/src/main/java/com/zhc/service/AddressService.java
@@ -0,0 +1,45 @@
+package com.zhc.service;
+
+import com.zhc.pojo.UserAddress;
+import com.zhc.pojo.bo.AddressBO;
+
+import java.util.List;
+
+public interface AddressService {
+
+    public List<UserAddress> queryAll(String userId);
+
+    /**
+     * 用户新增地址
+     * @param addressBO
+     */
+    public void addNewUserAddress(AddressBO addressBO);
+
+    /**
+     * 用户修改地址
+     * @param addressBO
+     */
+    public void updateUserAddress(AddressBO addressBO);
+
+    /**
+     * 根据用户id和地址id，删除对应的用户地址信息
+     * @param userId
+     * @param addressId
+     */
+    public void deleteUserAddress(String userId, String addressId);
+
+    /**
+     * 修改默认地址
+     * @param userId
+     * @param addressId
+     */
+    public void updateUserAddressToBeDefault(String userId, String addressId);
+
+    /**
+     * 根据用户id和地址id，查询具体的用户地址对象信息
+     * @param userId
+     * @param addressId
+     * @return
+     */
+    public UserAddress queryUserAddres(String userId, String addressId);
+}
